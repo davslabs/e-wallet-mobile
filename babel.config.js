c2a399d7f54ba66615950bcab72d3d2c62536ef8
David Ezequiel Assen <assen.david@gmail.com>
2022-10-16 21:26:02 -0300

    fix: refactor configuraciones de la app y scaffolding

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+const alias = require('./importAliases');
+
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias,
+          extensions: ['.jsx', 'js']
+        }
+      ]
+    ]
   };
 };
