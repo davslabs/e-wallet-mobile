e6bc271efc3d68bb67616337e8e932142659d96d
Javier Mondini <javiermondini@gmail.com>
2022-10-24 09:19:13 -0300

    fix: castear constantes de .env a string para evitar errores

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,12 +1,15 @@
 import axios from 'axios';
+
 import { API_BASE_URL } from '@env';
 
 export default axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: `${API_BASE_URL}`,
 });
 
+console.log(`BASE URL == ${API_BASE_URL}`);
+
 export const axiosPrivate = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: `${API_BASE_URL}`,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
