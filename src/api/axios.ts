1be34f0c26c4e21f52e61f7adbdf7025dfa55ce2
Javier Mondini <javiermondini@gmail.com>
2022-10-25 09:46:36 -0300

    style: limpiar console.log no necesario

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -6,8 +6,6 @@ export default axios.create({
   baseURL: `${API_BASE_URL}`,
 });
 
-console.log(`BASE URL == ${API_BASE_URL}`);
-
 export const axiosPrivate = axios.create({
   baseURL: `${API_BASE_URL}`,
   headers: { 'Content-Type': 'application/json' },
