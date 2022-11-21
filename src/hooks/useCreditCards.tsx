f808b526c3a5d6048b19992ddc546a13ea9b150b
Javier Mondini <javiermondini@gmail.com>
2022-11-21 16:19:28 -0300

    feat: agregar hook añadir tarjetas. agregar validacion de tarjetas

--- src/hooks/useCreditCards.tsx
+++ src/hooks/useCreditCards.tsx
@@ -21,16 +21,7 @@ export const useCreditCards = () => {
     }
   }, []);
 
-  const saveCard = async (card: NewCreditCard) => {
-    const CreditCardRegex = '(^4[0-9]{12}(?:[0-9]{3})?$)|(^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$)|(3[47][0-9]{13})|(^3(?:0[0-5]|[68][0-9])[0-9]{11}$)|(^6(?:011|5[0-9]{2})[0-9]{12}$)|(^(?:2131|1800|35\d{3})\d{11}$)';
-    const CVVRegex = '^[0-9]{3, 4}$';
-    if (!card.categoria) throw new Error('La categoría de la tarjeta no es válida');
-    if (!card.codigoDeSeguridad || !card.codigoDeSeguridad.match(CVVRegex)) throw new Error('el código de seguridad no es válido');
-    if (!card.fechaVencimiento) throw new Error('La fecha de vencimiento no es válida');
-    if (!card.numero || !card.numero.match(CreditCardRegex)) throw new Error('El numero de la tarjeta no es válido');
-    if (!card.tipo) throw new Error ('El tipo de la tarjeta no es válido');
-    if (!card.titular) throw new Error('Ingrese el nombre del titular de la tarjeta');
-
+  const saveCard = async (card: NewCreditCard) => { 
     setIsLoading(true);
     try {
       const response = await axios.post('/tarjeta', card,
@@ -45,6 +36,8 @@ export const useCreditCards = () => {
       } else {
         console.error(error.message);
       }
+    } finally{
+      setIsLoading(false);
     }
     return false;
   }
